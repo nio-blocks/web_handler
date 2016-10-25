6f4e8a2094caebd8ae62137a2920cca35481e05b
Chris Brackert <cbrackert@gmail.com>
2016-10-25 17:30:12 -0600

    Move integration tests to their own directory

--- integration_tests/test_json.py
+++ integration_tests/test_json.py
@@ -1,39 +1,31 @@
+import json
 import requests
 from collections import defaultdict
 from ..broker import RequestResponseBroker
-from ..web_handler_block import WebHandler
-from ..web_output_block import WebOutput
+from ..web_handler_block import WebJSONHandler
+from ..web_output_block import WebJSONOutput
 from unittest.mock import patch
 from nio.testing.block_test_case import NIOBlockTestCase
-from nioext.util.support import NIOExtModuleLocations
-from nioext.util.support.provider import NIOExtTestConfigurationProvider
 
 
-class TestRequests(NIOBlockTestCase):
+class TestJSONRequests(NIOBlockTestCase):
 
     def get_test_modules(self):
-        return super().get_test_modules() + ['web']
-
-    def get_module_locations(self):
-        return NIOExtModuleLocations
-
-    def get_test_configuration_provider(self):
-        return NIOExtTestConfigurationProvider
+        """ Adds 'web' and 'security' to default modules """
+        return super().get_test_modules() | {'web'}
 
     def setUp(self):
         super().setUp()
         self.last_notified = defaultdict(list)
         # Build the handler and output blocks
-        self.handler_block = WebHandler()
+        self.handler_block = WebJSONHandler()
         self.configure_block(self.handler_block, {
             'host': '127.0.0.1',
             'port': 8182
         })
 
-        self.output_block = WebOutput()
+        self.output_block = WebJSONOutput()
         self.configure_block(self.output_block, {
-            'response_out': '{{ $body }}',
-            'id_val': '{{ $id }}',
             'response_status': '200'
         })
 
@@ -55,30 +47,26 @@ class TestRequests(NIOBlockTestCase):
         else:
             return requests.request(method, url)
 
-    def test_get_basic(self):
-        self.run_basic_response('GET')
-
-    def test_post_basic(self):
-        self.run_basic_response('POST')
-
-    def test_put_basic(self):
-        self.run_basic_response('PUT')
-
-    def test_delete_basic(self):
-        self.run_basic_response('DELETE')
-
-    def run_basic_response(self, method):
+    def test_basic_json(self):
         """ Make sure that a basic chain of handler to output works """
 
+        json_obj = {
+            "json_key": {
+                "nested": "value"
+            }
+        }
         # Start by patching the register in the broker, and making a request
         with patch.object(RequestResponseBroker, 'wait_for_response') as reg:
-            self.make_request(method)
+            self.make_request('POST', json.dumps(json_obj))
             # Make sure our request was registered and that a signal
             # was notified
             self.assertEqual(reg.call_count, 1)
             self.assert_num_signals_notified(1, self.handler_block)
 
         the_sig = self.last_notified['default'][0]
+        # Make sure the signal's body came through as an actual dictionary
+        # and not a string due to the JSON handler being used
+        self.assertDictEqual(the_sig.json_key, {"nested": "value"})
 
         # Now patch the write response
         with patch.object(RequestResponseBroker, 'write_response') as write:
@@ -87,4 +75,17 @@ class TestRequests(NIOBlockTestCase):
 
             # Make sure write_response was called with the correct ID
             self.assertEqual(write.call_count, 1)
-            self.assertEqual(write.call_args[0][0], the_sig.id)
+            self.assertEqual(write.call_args[0][0], the_sig._id)
+            self.assertDictEqual(
+                json.loads(write.call_args[1]['body']), json_obj)
+            self.assertEqual(
+                write.call_args[1]['headers']['Content-Type'],
+                'application/json')
+
+    def test_bad_json_request(self):
+        """Make sure that bad JSON request doesn't make through the handler"""
+        with patch.object(RequestResponseBroker, 'wait_for_response') as reg:
+            self.make_request('POST', 'string not JSON')
+            # Make sure our request was registered and that a signal
+            # was notified
+            self.assertEqual(reg.call_count, 0)
--- integration_tests/test_requests.py
+++ integration_tests/test_requests.py
@@ -1,38 +1,34 @@
-import json
 import requests
 from collections import defaultdict
 from ..broker import RequestResponseBroker
-from ..web_handler_block import WebJSONHandler
-from ..web_output_block import WebJSONOutput
+from ..web_handler_block import WebHandler
+from ..web_output_block import WebOutput
 from unittest.mock import patch
 from nio.testing.block_test_case import NIOBlockTestCase
-from nioext.util.support import NIOExtModuleLocations
-from nioext.util.support.provider import NIOExtTestConfigurationProvider
+#from nioext.util.support import NIOExtModuleLocations
+#from nioext.util.support.provider import NIOExtTestConfigurationProvider
 
 
-class TestJSONRequests(NIOBlockTestCase):
+class TestRequests(NIOBlockTestCase):
 
     def get_test_modules(self):
-        return super().get_test_modules() + ['web']
-
-    def get_module_locations(self):
-        return NIOExtModuleLocations
-
-    def get_test_configuration_provider(self):
-        return NIOExtTestConfigurationProvider
+        """ Adds 'web' and 'security' to default modules """
+        return super().get_test_modules() | {'web', 'security'}
 
     def setUp(self):
         super().setUp()
         self.last_notified = defaultdict(list)
         # Build the handler and output blocks
-        self.handler_block = WebJSONHandler()
+        self.handler_block = WebHandler()
         self.configure_block(self.handler_block, {
             'host': '127.0.0.1',
             'port': 8182
         })
 
-        self.output_block = WebJSONOutput()
+        self.output_block = WebOutput()
         self.configure_block(self.output_block, {
+            'response_out': '{{ $body }}',
+            'id_val': '{{ $id }}',
             'response_status': '200'
         })
 
@@ -54,26 +50,30 @@ class TestJSONRequests(NIOBlockTestCase):
         else:
             return requests.request(method, url)
 
-    def test_basic_json(self):
+    def test_get_basic(self):
+        self.run_basic_response('GET')
+
+    def test_post_basic(self):
+        self.run_basic_response('POST')
+
+    def test_put_basic(self):
+        self.run_basic_response('PUT')
+
+    def test_delete_basic(self):
+        self.run_basic_response('DELETE')
+
+    def run_basic_response(self, method):
         """ Make sure that a basic chain of handler to output works """
 
-        json_obj = {
-            "json_key": {
-                "nested": "value"
-            }
-        }
         # Start by patching the register in the broker, and making a request
         with patch.object(RequestResponseBroker, 'wait_for_response') as reg:
-            self.make_request('POST', json.dumps(json_obj))
+            self.make_request(method)
             # Make sure our request was registered and that a signal
             # was notified
             self.assertEqual(reg.call_count, 1)
             self.assert_num_signals_notified(1, self.handler_block)
 
         the_sig = self.last_notified['default'][0]
-        # Make sure the signal's body came through as an actual dictionary
-        # and not a string due to the JSON handler being used
-        self.assertDictEqual(the_sig.json_key, {"nested": "value"})
 
         # Now patch the write response
         with patch.object(RequestResponseBroker, 'write_response') as write:
@@ -82,17 +82,4 @@ class TestJSONRequests(NIOBlockTestCase):
 
             # Make sure write_response was called with the correct ID
             self.assertEqual(write.call_count, 1)
-            self.assertEqual(write.call_args[0][0], the_sig._id)
-            self.assertDictEqual(
-                json.loads(write.call_args[1]['body']), json_obj)
-            self.assertEqual(
-                write.call_args[1]['headers']['Content-Type'],
-                'application/json')
-
-    def test_bad_json_request(self):
-        """Make sure that bad JSON request doesn't make through the handler"""
-        with patch.object(RequestResponseBroker, 'wait_for_response') as reg:
-            self.make_request('POST', 'string not JSON')
-            # Make sure our request was registered and that a signal
-            # was notified
-            self.assertEqual(reg.call_count, 0)
+            self.assertEqual(write.call_args[0][0], the_sig.id)
