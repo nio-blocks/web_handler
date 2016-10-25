62669bad500b254bf15b68323d1a67f779384a66
Chris Brackert <cbrackert@gmail.com>
2016-10-25 17:29:48 -0600

    Remove unused code in test_handler

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,22 +6,12 @@ from ..handler import Handler
 
 class TestHandler(NIOBlockTestCase):
 
-    def setUp(self):
-        super().setUp()
-        # This will keep a list of signals notified for each output
-        self.last_notified = defaultdict(list)
-
-    def signals_notified(self, signals, output_id='default'):
-        self.last_notified[output_id].extend(signals)
-
     def test_handler_post(self):
-        handler = Handler(endpoint='',
-                          blk=MagicMock())
+        handler = Handler(endpoint='', blk=MagicMock())
         handler.run_request = MagicMock()
         handler.on_post(MagicMock(), MagicMock())
         self.assertEqual(handler.run_request.call_args[0][0], 'POST')
 
     def test_handler_options(self):
-        handler = Handler(endpoint='',
-                          blk=MagicMock())
+        handler = Handler(endpoint='', blk=MagicMock())
         handler.on_options(MagicMock(), MagicMock())
